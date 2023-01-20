9511b7e2ad96cbe911251691981a0c4c1e8fd8a8
Flavio Mendes <flaviojmendes@gmail.com>
2023-01-20 15:50:39 +0000

    requirements

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 import dotenv
 from fastapi import FastAPI
+import uvicorn
 from models.plot_model import PlotModel
 from models.story_model import StoryModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -17,6 +19,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.post("/generate")
 async def generate(plot: PlotModel) -> StoryModel:
     return await generate_story(plot)
+
+if __name__ == '__main__':
+    if (os.environ["ENV"] == 'prod'):
+        uvicorn.run("main:app",
+                    host="0.0.0.0",
+                    port=8000,
+                    reload=True,
+                    ssl_keyfile="privkey.pem",
+                    ssl_certfile="cert.pem"
+                    )
+    else:
+        uvicorn.run("main:app",
+                    host="0.0.0.0",
+                    port=8000,
+                    reload=True
+                    )
